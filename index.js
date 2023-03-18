a83d5fcdf802148d6e6f0226ed623e125a464826
mammod2 <mammod26@gmail.com>
2023-03-18 12:39:56 +0800

    added save tab feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inputBtn = document.getElementById('input-btn')
 const deleteBtn = document.getElementById('delete-btn')
+const linkBtn = document.getElementById('link-btn')
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 let myLeads = []
@@ -11,15 +12,45 @@ deleteBtn.addEventListener('click' , function(event){
     location.reload()
 })
 
+linkBtn.addEventListener('click',function(event){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        let tab = tabs[0].url
+        myLeads.push(tab)
+        localStorage.setItem("myLeads" , JSON.stringify(myLeads))
+        location.reload()
+        console.log(myLeads);
+        // let activeTabId = activeTab.id // or do whatever you need
+      });
+    
 
+})
 
 
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    console.log("something should happen");
-    render()
+    render(myLeads)
     }
+function render(leads) {
+    let items =  ''
+    // let leadsFromLocalStorage = JSON.parse(localStorage.getItem("date"))
+   
+
+    for (let i = 0 ; i < leads.length ; i++ ){
+        items += `
+        <li class='p-2 m-2 rounded-md bg-white '>
+        <a target='_blank' href=' ${leads[i]}' class = 'border-b-2 border-blue-200 '> 
+        ${leads[i]}
+        <a> 
+        <li>
+        `
+    }
+    
+    
+    ulEl.innerHTML = items
+}
 
 inputEl.addEventListener("keypress" , function(event) {
     if (inputEl.value === ''){
@@ -39,26 +70,7 @@ function saveLead(){
     }
     const val = inputEl.value
     myLeads.push(val)
-    render()   
+    render(myLeads)   
     inputEl.value = ''
     localStorage.setItem("myLeads" , JSON.stringify(myLeads))
-}
-function render() {
-    let items =  ''
-    // let leadsFromLocalStorage = JSON.parse(localStorage.getItem("date"))
-   
-
-    for (let i = 0 ; i < myLeads.length ; i++ ){
-        items += `
-        <li class='p-2 m-2 rounded-md bg-white '>
-        <a target='_blank' href=' ${myLeads[i]}' class = 'border-b-2 border-blue-200 '> 
-        ${myLeads[i]}
-        <a> 
-        <li>
-        `
-    }
-    
-    console.log(myLeads);
-    
-    ulEl.innerHTML = items
 }
